48f9a4b005bb55048592be44a6ecbe7ee88c1ce8
pfanni00 <max.pfannenschmidt@gmx.de>
2024-03-21 15:09:59 +0100

    Erste arbeiten an DIalogsystem

---
+++ UnityProjekte/BachelorGame/BachelorGameMaxPfannenschmidt/Assets/Scripts/DialogsystemManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogsystemManager : MonoBehaviour
+{
+    int DialogState;
+    bool varianteSD;
+    // Controlliert ob variante Schlau oder Dumm aktiv ist.
+
+    bool ItemDialogoptionenAktiv
+    // erlaubt es ItemDialogoptionen 
+    GameObject DialogSystemUI;
+    GameObject DialogOption1a;
+    GameObjectDialogOption1b; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        DialogState = 1;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
