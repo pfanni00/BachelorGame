620100e6aade926ee78f292c76e44081edacde54
pfanni00 <max.pfannenschmidt@gmx.de>
2024-05-07 13:06:10 +0200

    Fixed The Raycasting problems

--- UnityProjekte/BachelorGame/BachelorGameMaxPfannenschmidt/Assets/Scripts/Interactor.cs
+++ UnityProjekte/BachelorGame/BachelorGameMaxPfannenschmidt/Assets/Scripts/Interactor.cs
@@ -10,49 +10,76 @@ interface IInteractable {
 }
 public class Interactor : MonoBehaviour
 {
-//    Dieses Script managed das interagieren mit Objecten im Level wie Türen oder Items 
-public Transform InteractorSource;
-public float InteractRange;
-private IInteractable currentInteractable; // Referenz auf das aktuelle IInteractable-Objekt
-private bool onHover;
+    //    Dieses Script managed das interagieren mit Objecten im Level wie Türen oder Items 
+    public Transform InteractorSource;
+    public float InteractRange;
+    private IInteractable currentInteractable; // Referenz auf das aktuelle IInteractable-Objekt
+    private bool onHover;
     private bool InteractActivated;
-public LayerMask DefaultLayer;
-public  LayerMask InteractableLayer;
+    public LayerMask DefaultLayer;
+    //public LayerMask InteractableLayer;
 
-void Start()
+    void Start()
     {
     }
 
     void Update()
     {
         Ray r = new Ray(InteractorSource.position, InteractorSource.forward);
-      
+
 
         if (Physics.Raycast(r, out RaycastHit hitInfo, InteractRange, DefaultLayer))
         {
-            if (hitInfo.collider.gameObject.TryGetComponent(out IInteractable interactObj))
+            GameObject hitObject = hitInfo.collider.gameObject;
+
+            // prüfung of hitObject Interactiv ist
+            if (hitObject.TryGetComponent(out IInteractable interactObj))
             {
-                currentInteractable = interactObj;
+                //if (!onHover)
+                //  {
+                //onHover = true;
+                // currentInteractable = interactObj;
 
                 if (Input.GetKeyDown(KeyCode.E))
                 {
-                    currentInteractable.Interact();
+                    //mit object kann interagiert werden
+                    interactObj.Interact();
                 }
-                
-                currentInteractable.HoverInteract();
-                onHover = true;
-        } else
+
+                //Hover Text wird eingeblendet
+                interactObj.HoverInteract();
+
+                if (!onHover)
+                {   //Hover wert wird gespeichert
+                    onHover = true;
+                    Debug.Log(onHover);
+                    currentInteractable = interactObj;
+                }
+            }
+            else
             {
+                // Keine Interaktion, falls es sich um kein interaktives Objekt handelt
                 if (onHover)
                 {
-                    currentInteractable.HoverInteractOFF();
+                    currentInteractable?.HoverInteractOFF();
                     onHover = false;
+                    Debug.Log(onHover);
+
                     currentInteractable = null;
                 }
             }
+        }
+        else
+        {
+            // Keine Interaktion, falls kein Object Gefunden wird 
+            if (onHover)
+            {
+                currentInteractable?.HoverInteractOFF();
+                onHover = false;
+                Debug.Log(onHover);
 
-
-           
+                currentInteractable = null;
+            }
         }
     }
-    }
+}
