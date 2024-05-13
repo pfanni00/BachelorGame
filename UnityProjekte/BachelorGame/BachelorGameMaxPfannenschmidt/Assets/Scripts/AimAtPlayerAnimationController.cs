4194eff3f3feb25579d5eb8dbcbd2bbe5bd0066f
pfanni00 <max.pfannenschmidt@gmx.de>
2024-05-13 18:58:50 +0200

    Aim At Player work in progress

--- UnityProjekte/BachelorGame/BachelorGameMaxPfannenschmidt/Assets/Scripts/AimAtPlayerAnimationController.cs
+++ UnityProjekte/BachelorGame/BachelorGameMaxPfannenschmidt/Assets/Scripts/AimAtPlayerAnimationController.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AimAtPlayerAnimationController : MonoBehaviour
-{
+{// dieses Script verändert die variablen velocityX und VelocityZ des Animators so das die Katze mit ihrem Blick dem Spieler folgt. Die Berechnung der Werte für die Blickdrehung der Katze findet im Script AimAtPlayerValueCalculator Statt
     Animator animator;
     public GameObject valueCalculator;
-    private float velocityxTarget;
-    private float velocityx;
+    private float velocityXTarget;
+    private float velocityX;
+    private float velocityZTarget;
+    private float velocityZ;
     public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        velocityx = 0;
-        velocityxTarget = 0;
+        velocityX = 0;
+        velocityXTarget = 0;
+        velocityZ = 0;
+        velocityZTarget = 0;
       animator = GetComponent<Animator>();
     }
 
@@ -21,19 +25,39 @@ public class AimAtPlayerAnimationController : MonoBehaviour
     void Update()
     {
         AimAtPlayerValueCalculator aimAtPlayerValueCalculator = valueCalculator.GetComponent<AimAtPlayerValueCalculator>();
-        velocityxTarget = aimAtPlayerValueCalculator.XPercent;
-        velocityxTarget = -1 * (velocityxTarget - 0.5f);
+        velocityXTarget = aimAtPlayerValueCalculator.XPercent;
+        velocityZTarget = aimAtPlayerValueCalculator.ZPercent;
 
-        if(velocityx < velocityxTarget )
-            {
-            velocityx += velocityxTarget * speed;
-            }
-        else if (velocityx > velocityxTarget ) 
-            {  
-            velocityx -= velocityxTarget * speed; 
-            }
-       // velocityx = Mathf.Lerp(velocityx, velocityxTarget, speed * Time.deltaTime);
-    
-    animator.SetFloat("VelocityX", velocityx);
+        // Prozent wird in Wert für velocityX umgerechnet
+        velocityXTarget = -1 * (velocityXTarget - 0.5f);
+
+        velocityZTarget = velocityZTarget * 0.8f;
+
+
+        velocityZ = Mathf.Lerp(velocityZ, velocityZTarget, speed * Time.deltaTime);
+        velocityX = Mathf.Lerp(velocityX, velocityXTarget, speed * Time.deltaTime);
+
+        //  velocityX wird auf ihre Maximal werte gesetzt sollte sie diese Überschreiten
+        if (velocityX > 0.5f)
+        {
+            velocityX = 0.5f;
+        }
+        if (velocityX < -0.5f)
+        {
+            velocityX = -0.5f;
+        }
+
+        // velocityZ wird auf ihre Maximal werte gesetzt sollte sie diese Überschreiten
+        if (velocityZ > 0.8f) 
+        {
+            velocityZ = 0.8f;
+        }
+        if (velocityZ < 0.45f)
+        {
+            velocityZ = 0.45f;
+        }
+
+        animator.SetFloat("VelocityX", velocityX);
+        animator.SetFloat("VelocityZ", velocityZ);
     }
 }
--- UnityProjekte/BachelorGame/BachelorGameMaxPfannenschmidt/Assets/Scripts/AimAtPlayerValueCalculator.cs
+++ UnityProjekte/BachelorGame/BachelorGameMaxPfannenschmidt/Assets/Scripts/AimAtPlayerValueCalculator.cs
@@ -14,24 +14,32 @@ public class AimAtPlayerValueCalculator : MonoBehaviour
     private float Zcordinate;
     public float XMax;
     public float XMin;
+    public float ZMax;
+    public float ZMin;
     public float XPercent;
+    public float ZPercent;
     // Start is called before the first frame update
     void Start()
     {
     }
 
-    // Update is called once per frame
     void OnTriggerStay(Collider PlayerObj)
     {
+        // Wenn der Spieler den Collider des Dialog Triggers berührt wird seine Position in diesem ermittelt und die X sowie Z werte jeweils in Prozent umgerechnet. Aus diesen Prozentwerten werden im Script AimAtPlayerAnimationController die Velocity Werte für die Kopfdrehung der Katze berechnet
         currentPosition = PlayerObj.transform.position;
         //Debug.Log(currentPosition);
         Xcordinate = currentPosition.x;
-        //Zcordinate = currentPosition.z;
+        Zcordinate = currentPosition.z;
 
-        float range = XMax - XMin;
-        float currentValue = XMax - Xcordinate;
-        float xpercent = currentValue / range;
+        float rangeX = XMax - XMin;
+        float currentValueX = XMax - Xcordinate;
+        float xpercent = currentValueX / rangeX;
         XPercent = (float)Math.Round(xpercent, 2);
+        
+        float rangeZ = ZMax - ZMin;
+        float currentValueZ = ZMax - Zcordinate;
+        float zpercent = currentValueZ / rangeZ;
+        ZPercent = (float)Math.Round(zpercent, 2);
 
         Debug.Log(XPercent);
 
